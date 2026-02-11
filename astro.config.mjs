6e68b5cb3359fc280a8e3b063ee886194dd355bd
Gustavo <gnk.github@proton.me>
2026-02-10 20:32:45 -0800

    Remove base path for root deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://gnabak.github.io',
-	base: '/dev-blog/',
 	integrations: [
 		tailwind(),
 		mdx(),
